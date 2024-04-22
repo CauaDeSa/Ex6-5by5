9097cfc0a124e866c60dc33d2c3ff58363210d69
Cauã Rufino de Sá <cauarufinodesa@gmail.com>
2024-04-22 16:36:08 -0300

    Reverting.

--- Ex6-5by5/Program.cs
+++ Ex6-5by5/Program.cs
@@ -29,25 +29,16 @@ do
     switch (bmi) {
         case (bmi < 20.7 && sex == "m") || (bmi < 19.1 && sex == "f"):
         Console.WriteLine("Below normal!");
-        break;
+    else if ((bmi < 26.4 && sex == "m") || (bmi < 25.8 && sex == "f"))
+            Console.WriteLine("Healthy range!");
+        else if ((bmi < 27.8 && sex == "m") || (bmi < 27.3 && sex == "f"))
+                Console.WriteLine("A little overweight!");
+            else if ((bmi < 32.3 && sex == "m") || (bmi < 31.1 && sex == "f"))
+                    Console.WriteLine("Overweight!");
+                else
+                    Console.WriteLine("Obesity range!");
 
-    case (bmi < 26.4 && sex == "m") || (bmi < 25.8 && sex == "f"):
-        Console.WriteLine("Healthy range!");
-        break;
-
-    case (bmi < 27.8 && sex == "m") || (bmi < 27.3 && sex == "f"):
-        Console.WriteLine("A little overweight!");
-        break;
-
-    case (bmi < 32.3 && sex == "m") || (bmi < 31.1 && sex == "f"))
-            Console.WriteLine("Overweight!");
-        break;
-
-    default:
-        Console.WriteLine("Obesity range!");
-        break;
-    }
-        Console.WriteLine($"Your BMI: {bmi:0.00}");
+    Console.WriteLine($"Your BMI: {bmi:0.00}");
 
         Console.Write("\nDo you wanna test again? (y/n) : ");
     } while (Console.ReadLine() == "y") ;
